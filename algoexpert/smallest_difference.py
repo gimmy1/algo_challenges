dfc08c0a0b0395ea9e91f4159ccd51010f3a91d0
Gimmy <gamalgiali@gmail.com>
2020-05-16 19:04:58 -0400

    algo fix: idiot fix

--- algoexpert/smallest_difference.py
+++ algoexpert/smallest_difference.py
@@ -1,33 +1,27 @@
-def smallest_difference(arr1, arr2):
-    sarr1 = sorted(arr1)
-    sarr2 = sorted(arr2)
-    a1 = 0
-    a2 = 0
-    small = float("inf")
-    difference = []
-    while a1 < len(arr1) or a1 < len(arr2):
-        import pdb; pdb.set_trace()
-        val = abs(sarr1[a1] - sarr2[a2])
-        if val < small:
-            small = val
-            try:
-                difference.pop()
-                difference.append((sarr1[a1], sarr2[a2]))
-            except:
-                difference.append((sarr1[a1], sarr2[a2]))
+def smallest_difference(array1, array2):
+	array1.sort()
+	array2.sort()
+	i1 = 0
+	i2 = 0
+	smallest = float("inf")
+	difference = []
 
-        if sarr1[a1] < sarr2[a2]:
-            a1 += 1
-        elif sarr1[a1] > sarr2[a2]:
-            a2 += 1
-        else:
-            return [sarr1[a1], sarr2[a2]]
-    
-    return difference
+	while i1 < len(array1) and i2 < len(array2): 
+		current = abs(array1[i1] - array2[i2])
+		f = array1[i1], s = array2[i2]
+		if array1[i1] < array2[i2]:
+			i1 += 1
+		elif array1[i1] > array2[i2]:
+			i2 += 1
+		else:
+			return [array1[i1], array2[i2]]
+		
+		if current < smallest:
+			smallest = current
+			difference = [f, s]
+	return difference
 
 
 
 if __name__ == "__main__":
-    arrayOne = [-1, 5, 10, 20, 28, 3]
-    arrayTwo = [26, 134, 135, 15, 17]
-    print(smallest_difference(arrayOne, arrayTwo))
+    print(smallest_difference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
