d94b2aebebcedf3fc0c2da884f888fb4a5ee1284
Gimmy <gamalgiali@gmail.com>
2020-05-19 14:23:03 -0400

    algo: redo bst construction

--- algoexpert/bst.py
+++ algoexpert/bst.py
@@ -1,86 +1,85 @@
 class Node:
-	def __init__(self, v):
-		self.v = v
-		self.left = None
-		self.right = None
-	
-	def insert(self, v):
-		if v < self.v:
-			if self.left:
-				return self.left.insert(v)
-			else:
-				self.left = Node(v)
-				return
-		else: # input value is larger
-			if self.right:
-				return self.right.insert(v)
-			else:
-				self.right = Node(v)
-				return
+    def __init__(self, v):
+        self.v = v
+        self.left = None
+        self.right = None
+    
+    def insert(self, v):
+        if v < self.v:
+            if self.left: 
+                return self.left.insert(v)
+            else:
+                self.left = Node(v)
+                return
+        else:
+            if self.right:
+                return self.right.insert(v)
+                
+            else:
+                self.right = Node(v)
+                return
+    
+    def search(self, v):
+        if v < self.v:
+            if self.left:
+                return self.left.search(v)
+        elif v > self.v:
+            if self.right:
+                return self.right.search(v)
+        return False
+        
+    def remove(self, v):
+        if v < self.v:
+            if self.left:
+                return self.left.remove(v)
+            else:
+                return None
+        elif v > self.v:
+            if self.right:
+                return self.right.remove(v)
+            else:
+                return None
+        
+        # CASES
+        if not self.left and not self.right:
+            return_val = self.v
+            self.v = None
+            return return_val
+        elif not self.left:
+            swap_val = self.right.v
+            return_val = self.v
+            self.v = swap_val
+            return return_val
+        elif self.left and self.right:
+            current = self.right
+            while current.left:
+                current = current.left
+            
+            return_val = self.v
+            self.v = current.v
+            return
 
-	def search(self, v):
-		if v < self.v:
-			if self.left:
-				return self.left.search(v)
-			else:
-				return False
-		else:
-			if self.right:
-				return self.right.search(v)
-			else:
-				return False
+        
 
-	def remove(self, v):
-		if v < self.v:
-			if self.left:
-				self.left.remove(v)
-			else:
-				return None
-		elif v > self.v:
-			if self.right:
-				self.right.remove(v)
-			else:
-				return None
-		
-		if self.left is None and self.right is None: # is a left Node
-			self.v = None
-		elif self.left is None: # only a right child
-			curr = self.v
-			self.v = self.right.v
-			return curr
-		elif self.right is None:
-			curr = self.v
-			self.v = self.left.v
-			return curr
-		else:
-			while self.right:
-				curr = self.right
-			self.v = curr.v
-			self.right = self.right.delete(curr.value)
-		return self
 
-class BST:
-	def __init__(self):
-		self.root = None
+class BinarySearchTree:
+    def __init__(self, v):
+        self.root = None
+    
+    def insert(self, v):
+        if self.root:
+            self.root.insert(v)
+            return
+        else:
+            self.root = Node(v)
+    
+    def search(self, v):
+        if self.root:
+            return self.root.search(v)
 
-	def insert(self, v):
-		if self.root:
-			return self.root.insert(v)
-		self.root = Node(v)
-		return
-	
-	def search(self, v):
-		if self.root:
-			return self.root.search(v)
-		return False
-
-	def remove(self, v):
-		# 6 scenarios
-		# a. no root
-		# b. no child
-		# c. child
-			# -> only left child
-			# -> only right child
-				# -> two children
-		if self.root:
-			self.root.remove(v)
+        else:
+            return False
+    
+    def remove(self, v):
+        if self.root:
+            return self.root.remove(v)
--- algoexpert/find_closest_value_bst.py
+++
@@ -1,5 +0,0 @@
-def find_closest_value():
-    pass
-
-def find_closest_helper():
-    pass
