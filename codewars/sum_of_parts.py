7c80ebeb5995cc527d72b18fea043301917eefad
Gimmy <gamalgiali@gmail.com>
2020-06-05 20:13:30 -0400

    back to algos

---
+++ codewars/sum_of_parts.py
@@ -0,0 +1,16 @@
+def parts_sums(ls):
+    # your code
+    i = len(ls) - 1
+    sums = [0] * (len(ls) + 1)
+    ji = len(sums) - 1
+    while ji > 0:
+        # OKAY THEN
+        # import pdb; pdb.set_trace()
+        parts = ls[i] + sums[ji]
+        sums[i] = parts
+        i -= 1
+        ji -= 1
+    return sums
+        
+if __name__ == "__main__":
+    print(parts_sums([0, 1, 3, 6, 10]))
