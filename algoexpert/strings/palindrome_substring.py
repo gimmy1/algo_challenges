b718a0a46becd6254e4b3a815253c0b700551a54
Gimmy <gamalgiali@gmail.com>
2020-06-16 21:35:41 -0400

    classic: palindromic substring

---
+++ algoexpert/strings/palindrome_substring.py
@@ -0,0 +1,19 @@
+def longest_palindromic_substring(string):
+    curr_longest = [0,1]
+    for i in range(1, len(string)):
+        odd = get_largest_palindrome(string, i-1, i+1)
+        even = get_largest_palindrome(string, i-1, i)
+        longest = max(odd, even, key=lambda x: x[1] - x[0])
+        curr_longest = max(curr_longest, longest, key=lambda x: x[1] - x[0])
+    return string[curr_longest[0]: curr_longest[1]]
+
+def get_largest_palindrome(string, left, right):
+    while left >= 0 and right < len(string):
+        if string[left] != string[right]:
+            break
+        left -= 1
+        right += 1
+    return [left+1, right]
+
+if __name__ == "__main__":
+    print(longest_palindromic_substring("abaxyzzyxf"))
