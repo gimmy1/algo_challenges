3d05c5c1228842b1c8b94a451a751d7dea549d1f
Gimmy <gamalgiali@gmail.com>
2020-11-15 14:15:49 -0500

    dynamic medium: min amt of coins

--- algoexpert/dynamic/medium/min_num_of_coins.py
+++ algoexpert/dynamic/medium/min_num_of_coins.py
@@ -1,5 +1,14 @@
 def min_num_of_coins(coins, target):
-    pass
+    num_of_coins = [float("inf") if amount != 0 else 0 for amount in range(target+1)]
+    for coin in coins:
+        for amount in range(target+1):
+            if coin <= amount:
+                # 0, 1, 2, 3, 4, 5, 6, 7
+                num_of_coins[amount] = min(num_of_coins[amount],
+                                            num_of_coins[amount-coin] + 1)
+    
+    return num_of_coins[target] if num_of_coins[target] != float("inf") else -1
+
 
 if __name__ == "__main__":
-    min_num_of_coins(1,1)
+    print(min_num_of_coins([1,5,10],7))
