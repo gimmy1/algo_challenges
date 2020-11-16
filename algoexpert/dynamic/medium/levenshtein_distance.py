942d595ca85800c189c8441f4c9565f04ae7eddd
Gimmy <gamalgiali@gmail.com>
2020-11-15 21:53:56 -0500

    dynamic medium: levenshtein distance done

--- algoexpert/dynamic/medium/levenshtein_distance.py
+++ algoexpert/dynamic/medium/levenshtein_distance.py
@@ -2,22 +2,22 @@ def levenshtein_distance(str1, str2):
     # abc
     # yabd
     edits = []
-    for i in range(len(str1) + 1):
+    for i in range(len(str2) + 1):
         inner = []
-        for j in range(len(str2)):
+        for j in range(len(str1) + 1):
             if j == 0 and i != 0:
                 inner.append(i)
             else:
                 inner.append(j)
         edits.append(inner)
-    
+
     
     for i in range(1, len(str2) + 1):
-        for j in range(len(str1) + 1):
-            if str2[i-1] != str1[j-1]:
-                edits[i][j] = edits[i-1][j-1]
+        for jx in range(1, len(str1) + 1):
+            if str2[i-1] == str1[jx-1]:
+                edits[i][jx] = edits[i-1][jx-1]
             else:
-                edits[i][j] = 1 + min(edits[i-1][j-1], edits[i-1][j], edits[i][j-1])
+                edits[i][jx] = 1 + min(edits[i-1][jx-1], edits[i-1][jx], edits[i][jx-1])
     return edits[-1][-1]
 
 if __name__ == "__main__":
